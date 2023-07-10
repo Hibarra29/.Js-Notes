69b0475aab7dc69b8b12a79a65cebfcc82dd2f2a
Hector <hmuralles07@gmail.com>
2023-07-10 17:07:59 -0500

    Added more code to index js in routesA

--- routes/routesA/index.js
+++ routes/routesA/index.js
@@ -12,3 +12,29 @@ router.get('/api/notes', (req, res) => {
     })
 })
 
+router.post('/api/notes', (req, res) => {
+    let newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    
+    //1. use fs to read the db.json
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+
+        let newData = JSON.parse(data);
+
+        newData.push(newNote);
+        console.log(newData)
+
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+
+            res.send('successfully added');
+        })
+    });
+
+})
+
+module.exports = router;
